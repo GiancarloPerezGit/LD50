78638bf41f3c1b243f328a8b6c693dd60b28e02f
Giancarlo Perez <42480241+GiancarloPerezGit@users.noreply.github.com>
2022-04-01 21:39:48 -0400

    Beginning turncontroller

---
+++ Assets/TurnController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnController : MonoBehaviour
+{
+    public int healthP = 4;
+    public int healthE = 4;
+    public int turn = 0;
+    public int pointsP;
+    public int pointsE;
+    private bool waitForAnimation = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(turn % 2 == 0)
+        {
+            if(!waitForAnimation)
+            {
+                endTurn();
+                waitForAnimation = true;
+            }    
+        }
+        else if(turn % 2 == 1)
+        {
+            healthE += 1;
+            endTurn();
+        }
+
+    }
+
+    IEnumerator animationPlay()
+    {
+       yield return new WaitForSeconds(1);
+        waitForAnimation = false;
+    }
+
+    public void actionSelected()
+    {
+        StartCoroutine(animationPlay());
+    }
+
+
+    public void endTurn()
+    {
+        turn += 1;
+        if(turn % 2 == 0)
+        {
+            addPoints();
+        }
+    }
+
+    public void addPoints()
+    {
+        pointsP += healthP;
+        pointsE += healthE;
+    }
+
+}
